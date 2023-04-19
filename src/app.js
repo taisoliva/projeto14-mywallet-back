a4ee5f713bc01381a8995973219b320dbac21356
taisoliva <tais15oliva@gmail.com>
2023-04-19 12:28:25 -0300

    Finished the general settings

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
 
 const app = express()
 const PORT = 5000
 
 app.use(express.json())
 app.use(cors())
+dotenv.config()
 
+let db
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+mongoClient.connect()
+    .then(() => {
+        db = mongoClient.db()
+    })
+    .catch((err) => console.log(err.message))
 app.listen(PORT, () => console.log(`Server Running in ${PORT}`))
